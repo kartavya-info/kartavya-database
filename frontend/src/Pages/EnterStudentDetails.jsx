7f7c4b5f7b18c39ee9c53a00ddcf49849927d13f
Amit <iamamitbhagat384@gmail.com>
2024-10-14 01:12:35 +0530

    Add an image in the form

--- frontend/src/Pages/EnterStudentDetails.jsx
+++ frontend/src/Pages/EnterStudentDetails.jsx
@@ -1,28 +1,42 @@
-import React, { useState } from 'react'
-import HeaderForEnputStudentDetails from '../components/HeaderForEnputStudentDetails'
+import React, { useState } from 'react';
+import HeaderForEnputStudentDetails from '../components/HeaderForEnputStudentDetails';
 import EnterStudentDetails1 from './EnterStudentDetails1';
 import EnterStudentDetails2 from './EnterStudentDetails2';
 
-
-
 const EnterStudentDetails = () => {
-
   const [isFirstPage, setIsFirstPage] = useState(true);
 
   const handleClick = () => {
     setIsFirstPage((prev) => !prev);
-  }
+  };
 
   return (
-    <div className='w-screen'>
+    <div className="w-screen">
       <HeaderForEnputStudentDetails />
 
-      {isFirstPage ? <EnterStudentDetails1 /> : <EnterStudentDetails2 />}
+      <div className="flex flex-col w-full xl:flex-row xl:h-[calc(100vh-7rem)]">
+        <div className="flex justify-center items-center w-full h-[500px] xl:w-2/5 xl:h-full">
+          <img
+            src="/girl_photo.png"
+            alt="girl_photo"
+            className="object-contain h-4/5"
+          />
+        </div>
 
-      <button onClick={() => {handleClick()}}> {isFirstPage? 'Next' : 'Back'} </button>
-      
+        <div className="w-full h-[500px] xl:w-3/5 xl:h-full">
+          {isFirstPage ? <EnterStudentDetails1 /> : <EnterStudentDetails2 />}
+        </div>
+      </div>
+
+      {/* <button
+        onClick={() => {
+          handleClick();
+        }}
+      >
+        {isFirstPage ? 'Next' : 'Back'}{' '}
+      </button> */}
     </div>
-  )
-}
+  );
+};
 
-export default EnterStudentDetails
+export default EnterStudentDetails;
--- frontend/src/Pages/EnterStudentDetails1.jsx
+++ frontend/src/Pages/EnterStudentDetails1.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 
 const EnterStudentDetails1 = () => {
   return (
-    <div>
-      <h2>This is EnterStudentDetails1</h2>
+    <div className='w-full h-full bg-blue-500'>
     </div>
   )
 }
