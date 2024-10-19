ecdfbe50efb6a0d199ecb95f92c6c8bcf49456be
Amit <iamamitbhagat384@gmail.com>
2024-10-19 10:35:04 +0530

    Add document details section to student-profile-page

--- frontend/src/Pages/StudentProfile.jsx
+++ frontend/src/Pages/StudentProfile.jsx
@@ -1,5 +1,6 @@
 import InputComponent from '@/components/InputComponent';
 import SelectComponent from '@/components/SelectComponent';
+import { Checkbox } from '@/components/ui/checkbox';
 import React, { useState } from 'react';
 
 const studentDataFromBackend = {
@@ -63,12 +64,16 @@ const StudentProfile = () => {
           </div>
         </div>
       </div>
-      <div className="hero w-[80%] h-[200vh] bg-fixed inset-0 bg-[radial-gradient(ellipse_at_center,rgba(222,80,85,0.4),transparent),radial-gradient(ellipse_at_top_left,rgba(205,214,219,0.8),rgba(255,255,255,0.8),rgba(255,255,255,0)),radial-gradient(ellipse_at_top_right,rgba(205,214,219,0.8),rgba(255,255,255,0.8),rgba(255,255,255,0)),radial-gradient(ellipse_at_bottom_left,rgba(205,214,219,0.8),rgba(255,255,255,0.8),rgba(255,255,255,0)),radial-gradient(ellipse_at_bottom_right,rgba(205,214,219,0.8),rgba(255,255,255,0.8),rgba(255,255,255,0))]">
+      <div className="hero flex flex-col gap-10 w-[80%] h-[200vh] bg-fixed inset-0 bg-[radial-gradient(ellipse_at_center,rgba(222,80,85,0.4),transparent),radial-gradient(ellipse_at_top_left,rgba(205,214,219,0.8),rgba(255,255,255,0.8),rgba(255,255,255,0)),radial-gradient(ellipse_at_top_right,rgba(205,214,219,0.8),rgba(255,255,255,0.8),rgba(255,255,255,0)),radial-gradient(ellipse_at_bottom_left,rgba(205,214,219,0.8),rgba(255,255,255,0.8),rgba(255,255,255,0)),radial-gradient(ellipse_at_bottom_right,rgba(205,214,219,0.8),rgba(255,255,255,0.8),rgba(255,255,255,0))]">
         <div className="heading text-3xl font-semibold text-center pt-5 pb-5 border-b w-full">
           Student Profile
         </div>
+
+        {/* General Details */}
         <div className="general-details scale-90">
-          <div className='w-full text-2xl font-semibold text-[#21526E]'> General Details</div>
+          <div className="w-full text-2xl font-semibold text-[#21526E]">
+            General Details
+          </div>
           <div className="input-section flex flex-col items-end w-full pt-8 gap-5">
             {/* Row 1 */}
             <div className="flex flex-col gap-5 xl:gap-0 xl:flex-row flex-wrap w-full">
@@ -221,6 +226,129 @@ const StudentProfile = () => {
                 />
               </div>
             </div>
+          </div>
+        </div>
+
+        {/* Document Details */}
+
+        <div className="document-details flex flex-col w-full gap-5 scale-90">
+          <div className="w-full text-2xl font-semibold text-[#21526E] mb-5">
+            Document Details
+          </div>
+          {/* Aadhar Details */}
+          <div className="flex items-center space-x-2">
+            <Checkbox
+              id="aadhar"
+              checked={studentData.aadhar}
+              onCheckedChange={(checked) =>
+                handleInputChange({
+                  target: { name: 'aadhar', type: 'checkbox', value: checked },
+                })
+              }
+            />
+            <label
+              htmlFor="aadhar"
+              className="text-lg font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >
+              Aadhar Card
+            </label>
+          </div>
+
+          {/* Domicile Details */}
+          <div className="flex items-center space-x-2">
+            <Checkbox
+              id="domicile"
+              checked={studentData.domicile}
+              onCheckedChange={(checked) =>
+                handleInputChange({
+                  target: { name: 'domicile', type: 'checkbox', value: checked },
+                })
+              }
+            />
+            <label
+              htmlFor="domicile"
+              className="text-lg font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >
+              Domicile Certificate
+            </label>
+          </div>
+
+
+          {/* Birth Certificate Details */}
+          <div className="flex items-center space-x-2">
+            <Checkbox
+              id="domicile"
+              checked={studentData.birthCertificate}
+              onCheckedChange={(checked) =>
+                handleInputChange({
+                  target: { name: 'birthCertificate', type: 'checkbox', value: checked },
+                })
+              }
+            />
+            <label
+              htmlFor="birthCertificate"
+              className="text-lg font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >
+              Birth Certificate
+            </label>
+          </div>
+
+
+          {/* Disability  Details */}
+          <div className="flex items-center space-x-2">
+            <Checkbox
+              id="disability"
+              checked={studentData.birthCertificate}
+              onCheckedChange={(checked) =>
+                handleInputChange({
+                  target: { name: 'disability', type: 'checkbox', value: checked },
+                })
+              }
+            />
+            <label
+              htmlFor="disability"
+              className="text-lg font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >
+              Disability Certificate
+            </label>
+          </div>
+
+          {/* Single Parent  Details */}
+          <div className="flex items-center space-x-2">
+            <Checkbox
+              id="singleParent"
+              checked={studentData.singleParent}
+              onCheckedChange={(checked) =>
+                handleInputChange({
+                  target: { name: 'singleParent', type: 'checkbox', value: checked },
+                })
+              }
+            />
+            <label
+              htmlFor="singleParent"
+              className="text-lg font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >
+              Single Parent Certificate
+            </label>
+          </div>
+
+          {/* Relevant Ceritificate  Details */}
+          <div className="flex items-center space-x-2">
+            <Checkbox
+              id="releventCertificate"
+              checked={studentData.releventCertificate}
+              onCheckedChange={(checked) =>
+                handleInputChange({
+                  target: { name: 'releventCertificate', type: 'checkbox', value: checked },
+                })
+              }
+            />
+            <label
+              htmlFor="releventCertificate"
+              className="text-lg font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >
+              Relevent Certificate for single parent
+            </label>
           </div>
         </div>
       </div>
