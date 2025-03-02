31a10579b8bb100f68a4307ba8dabc321c0056bc
Rajpd2003 <102207892+Rajpd2003@users.noreply.github.com>
2025-03-02 22:48:30 +0530

    Update csaRoutes.js

--- Backend/routes/csaRoutes.js
+++ Backend/routes/csaRoutes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const csaController = require("../controllers/csaController.js");
 
-// router.route("/").get(csaController.getVerifiedDonations);
-
-router.route("/:sponsorId").get(csaController.allotChild);
+router.route("/").get(csaController.getVerifiedDonations);
+router.route("/action").get(csaController.getChildTobeAlloted);
+router.route("/allot").patch(csaController.allotChild);
 
 module.exports = router;
